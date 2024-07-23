e4ce50d2ae09de482836ddd6b0ffe4f1467010c2
KwonSeongHyeok <aiqksiwk@naver.com>
2024-07-23 12:47:22 +0900

    tosspay backend

---
+++ Spting-tosspay-backend/src/main/java/tosspay/SptingTosspayBackendApplication.java
@@ -0,0 +1,13 @@
+package tosspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingTosspayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptingTosspayBackendApplication.class, args);
+	}
+
+}

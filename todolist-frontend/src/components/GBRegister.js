daf77218b9f3bcfa1e2fae3febf527f0326b07da
KwonSeongHyeok <aiqksiwk@naver.com>
2024-07-25 17:30:23 +0900

    MySQL-Gradle 연결 front

--- todolist-frontend/src/components/GBRegister.js
+++ todolist-frontend/src/components/GBRegister.js
@@ -4,7 +4,7 @@ const GBRegister = () => {
     
     const[GBPostTitle, setGBPostTitle] = useState('');
     const[GBPostContent, setGBPostContent] = useState('');
-    const[GBPostImages, setGBPostImages] = useState('');
+    const[GBPostImages, setGBPostImages] = useState();
 
     const[result, setResult] = useState('');
 
@@ -53,23 +53,24 @@ const GBRegister = () => {
 
     return (
         <div className="GB-container">
-            <label> Title :
-                <input type="text"
-                        value={GBPostTitle}
-                        onChange={e => setGBPostTitle(e.target.value)}
-                        required/>
-            </label>
-            <label> Content :
-                <textarea value={GBPostContent}
-                        onChange={e => setGBPostContent(e.target.value)}
-                        required
-                ></textarea>
-            </label>
-            <label> images :
-                <input type="file" accept="image/*" onChange={handleImageChange} multiple />
-            </label>
-            <button onClick={게시글등록}>게시글 등록하기</button>
-            {GBPostImages && (
+            <form onSubmit={(e) => handleOnSubmit(e)}>
+                <label> Title :
+                    <input type="text"
+                            value={GBPostTitle}
+                            onChange={e => setGBPostTitle(e.target.value)}
+                            required/>
+                </label>
+                <label> Content :
+                    <textarea value={GBPostContent}
+                            onChange={e => setGBPostContent(e.target.value)}
+                            required
+                    ></textarea>
+                </label>
+                <label> images :
+                    <input type="file" accept="image/*" onChange={handleImageChange} multiple />
+                </label>
+                <button onClick={게시글등록}>게시글 등록하기</button>
+                {GBPostImages && (
                 <div>
                     <h2>미리보기</h2>
                     {GBPostImages.map(img => (
@@ -77,6 +78,7 @@ const GBRegister = () => {
                     ))}
                 </div>
             )}
+            </form>  
         </div>
     );
 }

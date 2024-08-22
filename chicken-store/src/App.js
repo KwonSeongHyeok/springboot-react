6c1d639a84c57a04eacad25b1ca5f46991e4c296
KwonSeongHyeok <aiqksiwk@naver.com>
2024-08-22 12:05:11 +0900

    치킨 메뉴 수정 삭제

--- chicken-store/src/App.js
+++ chicken-store/src/App.js
@@ -1,32 +1,25 @@
-import { useState } from 'react';
-import './App.css';
-import ChickenForm from './component/ChickenForm';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ChickenList from './component/ChickenList';
-import Modal from './component/Modal';
+import ChickenDetail from './component/ChickenDetail';
+import MainRouter from './MainRouter.js';
+/*
+ChickenList path="/"
+
+ChickenDetail.js path="/chicken-detail"
+
+
+*/
 
 function App() {
-  const[isModalOpen, setIsModalOpen] = useState(false);
-  // 사용자가 보고싶을 때 볼 수 있도록 처음에는 false 보이지 않음 설정
 
-  // 오픈 true 닫음 false
-  // const에서 동작하는 기능이 1개일 때 {} 중괄호 생략 가능
-  const openModal = () => setIsModalOpen(true);
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  }
-
-  return (
-    <div className="App">
-        <ChickenList />
-
-
-        <button onClick={openModal}>메뉴등록하기</button>
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
-          <ChickenForm />
-        </Modal>
-    </div>
-  );
+    return(
+        <Router>
+            <Routes>
+                <Route path='/' element={<MainRouter />} />
+                {/* Routes 안에는 Route로 설정된 태그만 들어올 수 있음 <MainRouter /> */}
+                <Route path='/chicken-detail/:id' element={<ChickenDetail />} />
+            </Routes>
+        </Router>
+    )
 }
-
 export default App;
---
+++ chicken-store/src/MainRouter.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+import ChickenForm from './component/ChickenForm';
+import ChickenList from './component/ChickenList';
+import Modal from './component/Modal';
+
+const MainRouter = () => {
+  const[isModalOpen, setIsModalOpen] = useState(false);
+  // 사용자가 보고싶을 때 볼 수 있도록 처음에는 false 보이지 않음 설정
+
+  // 오픈 true 닫음 false
+  // const에서 동작하는 기능이 1개일 때 {} 중괄호 생략 가능
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
+
+  return (
+    <div className="app-container">
+      <h1>치킨 가게 메뉴 관리</h1>
+      <button onClick={openModal} className='chicken-register-button'>메뉴등록하기</button>
+        <ChickenList />
+
+
+        
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
+          <ChickenForm />
+        </Modal>
+    </div>
+  );
+}
+
+export default MainRouter;
